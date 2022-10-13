91ddabcbf85b0ab2ae983bd30067e995858eab13
McTawa <markmunyaka@gmail.com>
2022-10-13 22:30:02 +0000

    installed transformer

---
+++ src/backend/config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+   'transformer': {
+      enabled: true,
+      config: {
+        prefix: '/api/',
+        responseTransforms: {
+          removeAttributesKey: true,
+          removeDataKey: true,
+        }
+      }
+    },
+  });
