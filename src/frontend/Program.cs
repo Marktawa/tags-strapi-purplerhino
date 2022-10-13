c43a0ecf67e89783924e8c99d716364ea7fe0049
McTawa <markmunyaka@gmail.com>
2022-10-13 12:00:07 +0000

    Integrate Strapi API with Blazor App

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -1,3 +1,4 @@
+/*
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using frontend;
@@ -9,3 +10,42 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+*/
+
+// ./Program.cs
+
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using frontend;
+using frontend.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace frontend
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            // Example of loading a configuration as configuration isn't available yet at this stage.
+            services.AddSingleton(provider =>
+            {
+                var config = provider.GetService<IConfiguration>();
+                return config.GetSection("App").Get<AppSettings>();
+            });
+        }
+    }
+}
